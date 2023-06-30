6c684c2c529fedff24f7708e5e797d8c6faacc83
Gabriele Rundo <gabriele.rundo@studenti.unimi.it>
2023-06-30 12:29:06 +0000

    Informazioni studente

--- studente.php
+++ studente.php
@@ -1,11 +1,15 @@
 <?php
     ini_set("display_errors", "On");
 	ini_set("error_reporting", E_ALL);
+	include_once('lib/db_functions.php');
 
 	session_start();
 
     $logged = null;
+	$student = array();
     $logout_link = $_SERVER['PHP_SELF']."?log=del";
+	$path = "SET search_path=uni;";
+	$db = open_pg_connection();
 
     if(isset($_SESSION['email'])) {
 		$logged = $_SESSION['email'];
@@ -15,6 +19,30 @@
 		unset($_SESSION['email']);
 		$logged = null;
 	}
+
+	if(isset($logged)) {
+		pg_exec($db, $path);
+		$sql = "SELECT matricola, nome, cognome, corso_laurea 
+			FROM studente
+			WHERE email = $1";
+		$params = array($logged);
+
+		$result = pg_prepare($db, "student_description", $sql);
+		$result = pg_execute($db, "student_description", $params);
+
+		if($result) {
+			while($row = pg_fetch_assoc($result)) {
+				$student = array(
+					$row['matricola'],
+					$row['nome'],
+					$row['cognome'],
+					$row['corso_laurea']
+				);
+			}
+		}
+	}
+
+	close_pg_connection($db);
 ?>
 
 <!doctype html>
@@ -30,11 +58,44 @@
 		<h1 class="pb-2 border-bottom text-center">Pagina principale dello studente</h1>
 		<?php if(isset($logged)) { ?>
 			<div class="container mt-5">
-				<div class="alert alert-success text-center" role="alert">
+				<div class="alert alert-success text-center border" role="alert">
 				<p>
 					<?php echo("Benvenuto studente $logged, se vuoi effetuare il logout clicca ");?>
 					<a href="<?php echo $logout_link;?>">qui</a>
 				</p>
+			</div>
+			<div class="container mt-5">
+				<table class="table table-striped">
+					<h3 class="text-center">Informazioni dello studente</h3>
+					<thead>
+						<tr>
+							<th scope="col">Informazione</th>
+							<th scope="col">Valore</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">Matricola</th>
+							<td><?php echo $student[0]; ?></td>
+						</tr>
+						<tr>
+							<th scope="row">Nome</th>
+							<td><?php echo $student[1]; ?></td>
+						</tr>
+						<tr>
+							<th scope="row">Cognome</th>
+							<td><?php echo $student[2]; ?></td>
+						</tr>
+						<tr>
+							<th scope="row">Email</th>
+							<td><?php echo $logged; ?></td>
+						</tr>
+						<tr>
+							<th scope="row">Corso di laurea</th>
+							<td><?php echo $student[3]; ?></td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 	<?php } ?>
 	</div>
